da8fcfc50d7ba1ce435935ab84fe7931fa2cbb32
NSC508 <sathvikchinta@gmail.com>
2021-12-29 20:31:58 -0800

    Added rough implementation of all methods

--- src/Board.java
+++ src/Board.java
@@ -1,13 +1,15 @@
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Arrays;
+import java.util.StringJoiner;
 
 public class Board {
 
     public static final int AI_NUMBER = 1;
     public static final int PLAYER_NUMBER = 2;
+    public static final int MAX_WINNING_SCORE = Integer.MAX_VALUE;
+    public static final int MIN_WINNING_SCORE = Integer.MIN_VALUE;
     public int length; 
     public int height; 
-    int[] board;
+    int[][] board;
     
     public Board() {
         this(7, 6);
@@ -16,33 +18,32 @@ public class Board {
     public Board(int length, int height) {
         this.length = length;
         this.height = height;
-        this.board = new int[length * height];
+        this.board = new int[height][length];
     }
 
     @Override
     public String toString() {
-        String boardAsString = "";
-        for (int i = 0; i < board.length; i++) {
-            boardAsString += board[i];
-            if (i % this.length == this.length- 1) {
-                boardAsString += "\n";
-            }
+        StringJoiner sj = new StringJoiner(System.lineSeparator());
+        for (int[] row : this.board) {
+            sj.add(Arrays.toString(row));
         }
-        return boardAsString;
+        String result = sj.toString();
+        return result;
     }
 
     public boolean isWinningBoard() {
-        Set<Integer> seenIndices = new HashSet<Integer>();
         for (int i = 0; i < this.board.length; i++) {
-            if (board[i] != 0 && !seenIndices.contains(i)) {
-                if (board[i] == PLAYER_NUMBER) {
-                    isHorizontal(i, seenIndices, false);
-                    isVertical(i, seenIndices, false);
-                    isDiagonal(i, seenIndices, false);
-                } else {
-                    isHorizontal(i, seenIndices, true);
-                    isVertical(i, seenIndices, true);
-                    isDiagonal(i, seenIndices, true);
+            for (int j = 0; j < this.board[i].length; j++) {
+                if (board[i][j] != 0) {
+                    if (board[i][j] == PLAYER_NUMBER) {
+                        return isHorizontal(i, j, false) ||
+                        isVertical(i, j, false) ||
+                        isDiagonal(i, j, false);
+                    } else {
+                        return isHorizontal(i, j, true) ||
+                        isVertical(i, j, true) ||
+                        isDiagonal(i, j, true);
+                    }
                 }
             }
         }
@@ -50,7 +51,72 @@ public class Board {
     }
 
     public int heuristic() {
-        return 0; 
+        int vertical_points=0, horizontal_points=0, descDiagonal_points=0, ascDiagonal_points=0, total_points=0;
+
+        for (int row = 0; row < this.height - 3; row++) {
+            for (int column = 0; column < this.length; column++) {
+                int tempScore = calcScorePosition(row, column, 1, 0);
+                vertical_points += tempScore;
+                if(tempScore >= MAX_WINNING_SCORE || tempScore <= MIN_WINNING_SCORE)
+                    return tempScore;
+            }
+        }
+
+        for (int row = 0; row < this.height ; row++) {
+            for (int column = 0; column < this.length - 3; column++) {
+                int tempScore = calcScorePosition(row, column, 0, 1);
+                horizontal_points += tempScore;
+                if(tempScore >= MAX_WINNING_SCORE || tempScore <= MIN_WINNING_SCORE)
+                    return tempScore;
+            }
+        }
+
+        for (int row = 0; row < this.height - 3 ; row++) {
+            for (int column = 0; column < this.length - 3; column++) {
+                int tempScore = calcScorePosition(row, column, 1, 1);
+                descDiagonal_points += tempScore;
+                if(tempScore >= MAX_WINNING_SCORE || tempScore <= MIN_WINNING_SCORE)
+                    return tempScore;
+            }
+        }
+
+        for (int row = 3; row < this.height  ; row++) {
+            for (int column = 0; column < this.length - 4; column++) {
+                int tempScore = calcScorePosition(row, column, -1, 1);
+                ascDiagonal_points += tempScore;
+                if(tempScore >= MAX_WINNING_SCORE || tempScore <= MIN_WINNING_SCORE)
+                    return tempScore;
+            }
+        }
+
+        total_points = vertical_points + horizontal_points + descDiagonal_points + ascDiagonal_points;
+        return total_points;
+    }
+
+    private int calcScorePosition(int row, int column, int increment_row, int increment_col) {
+        int ai_points = 0, player_points = 0;
+
+        for (int i = 0; i < 4; i++) //connect "4"
+        {
+            if(board[column][row] == AI_NUMBER)
+            {
+                ai_points++;
+            }
+            else if (board[column][row] == PLAYER_NUMBER)
+            {
+                player_points++;
+            }
+
+            row += increment_row;
+            column += increment_col;
+        }
+
+        if(player_points == 4)
+            return MIN_WINNING_SCORE;
+        else if(ai_points == 4)
+            return MAX_WINNING_SCORE;
+        else
+            return ai_points;
     }
 
     public Board makeCopy() {
@@ -59,27 +125,86 @@ public class Board {
     }
 
     public void makeMove(int col, boolean isAI) {
-        int location = (this.length * (this.height - 1)) + col;
-        while (this.board[location] != 0 && (location > 0)) {
-            location -= this.length;
+        int currentHeight = 0;
+        while (currentHeight < this.height - 1) {
+            if (this.board[(this.height - 1) - currentHeight][col] == 0) {
+                if (isAI) {
+                    this.board[(this.height - 1) - currentHeight][col] = AI_NUMBER;
+                } else {
+                    this.board[(this.height - 1) - currentHeight][col] = PLAYER_NUMBER;
+                }
+                break;
+            }
+            currentHeight++;
         }
+    }
+
+    private boolean isHorizontal(int i, int j, boolean isAI) {
+        int playerNum;
         if (isAI) {
-            board[location] = AI_NUMBER;
+            playerNum = AI_NUMBER;
         } else {
-            board[location] = PLAYER_NUMBER;
+            playerNum = PLAYER_NUMBER;
         }
+        //Chcek 4 to the left and right to see if we find anything that is not the player
+        for (int row = 0; row < 4; row++) {
+            if (j + row < this.length) {
+                if (this.board[i][j + row] != playerNum) {
+                    return false;
+                }
+            } 
+            if (j - row > 0) {
+                if (this.board[i][j - row] != playerNum) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
-    private boolean isHorizontal(int index, Set<Integer> seenIndices, boolean isAI) {
-        return false;
+    private boolean isVertical(int i, int j, boolean isAI) {
+        int playerNum;
+        if (isAI) {
+            playerNum = AI_NUMBER;
+        } else {
+            playerNum = PLAYER_NUMBER;
+        }
+        //Check 4 on top and bottom to see if we find anything that is not the player
+        for (int col = 0; col < 4; col++) {
+            if (i + height < this.height) {
+                if (this.board[i + col][j] != playerNum) {
+                    return false;
+                }
+            } 
+            if (i - col > 0) {
+                if (this.board[i - col][j] != playerNum) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
-    private boolean isVertical(int index, Set<Integer> seenIndices, boolean isAI) {
-        return false;
+    private boolean isDiagonal(int i, int j, boolean isAI) {
+        int playerNum;
+        if (isAI) {
+            playerNum = AI_NUMBER;
+        } else {
+            playerNum = PLAYER_NUMBER;
+        }
+        //Check diagonally left and right to see if we find anything that is not the player
+        for (int diag = 0; diag < 4; diag++) {
+            if (i + diag < this.height && j + diag < this.length) {
+                if (this.board[i + diag][j + diag] != playerNum) {
+                    return false;
+                }
+            }
+            if (i - diag > 0 && j - diag  > 0) {
+                if (this.board[i - diag][j - diag] != playerNum) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
-
-    private boolean isDiagonal(int index, Set<Integer> seenIndices, boolean isAI) {
-        return false;
-    }
-
 }
--- src/main.java
+++ src/main.java
@@ -1,6 +1,8 @@
 public class main{
     public static void main(String[] args) {
         Board connect4Board = new Board(5, 6);
+        connect4Board.makeMove(0, true);
+        connect4Board.makeMove(0, true);
         System.out.println(connect4Board);
     }
 }
